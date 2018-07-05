c90e75376f5f0573611e2aca67c483a31781e6af
‫تكنو حامد -Techno Hamed‬‎ <39957737+TechnoHamed@users.noreply.github.com>
2018-07-05 19:38:26 +0200

    Update ahmed.js

--- ahmed.js
+++ ahmed.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function()
 {
-   if($("#blogID").attr("blogID")!="xxxxxx" )
+   if($("#blogID").attr("blogID")!="1383781597668434358" )
  {
    window.location.href="https://picalica.com/u/SOUHAILLEKHOUAJA/";
  }
