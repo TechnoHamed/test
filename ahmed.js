59737f975539729e8c0e1292c519a12eaba96fa9
‫تكنو حامد -Techno Hamed‬‎ <39957737+TechnoHamed@users.noreply.github.com>
2018-07-06 19:31:46 +0200

    Update ahmed.js

--- ahmed.js
+++ ahmed.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function()
 {
-   if($("#blogID").attr("blogID")!="1383781597668434358" )
+   if($("#blogID").attr("blogID")!="138378159@7668434358" )
    {
    window.location.href="https://www.technohamed.com/";
  }
